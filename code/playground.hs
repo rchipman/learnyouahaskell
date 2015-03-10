32cf8154456685e4f2d1189dd6e5f78244bab8b3
Rob Chipman <jrchipman1@gmail.com>
2015-03-10 11:16:55 -0400

    day 1

--- code/playground.hs
+++ code/playground.hs
@@ -22,10 +22,71 @@ removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 factorial :: Integer -> Integer
-factorial n = product [1..n]
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
 
-name :: Type
- pattern${3/.+/ /}${4/.+/= /}definition
+lucky :: (Integral a) => a -> String
+lucky 7 = "Lucky Number Seven!"
+lucky x = "Sorry, you're out of luck pal"
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head' on an empty list"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long, here are the first two elements: " ++ show x ++ " and " ++ show y
+
+lengthRecursive :: (Num b) => [a] -> b
+lengthRecursive [] = 0
+lengthRecursive (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum xs
+
+capital :: String -> String
+capital "" = "Empty String, whoops!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmi :: Float -> Float -> Float
+bmi x y = (x / (y * y)) * 703
+
+bmiTell :: Float -> Float -> String
+bmiTell weight height
+    | bmi <= skinny = "You are underweight, eat some food."
+    | bmi <= normal = "You are normal and healthy, nice work."
+    | bmi <= fat = "You are bit overweight, might want to switch to salads."
+    | otherwise = "You are morbidly obese, stop drinking sugary sodas and go for a 45 min walk each day."
+    where bmi = (weight / height ^ 2) * 730
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b     = a
+    | otherwise = b
+
+compare' :: (Ord a) => a -> a -> Ordering
+a `compare'` b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
